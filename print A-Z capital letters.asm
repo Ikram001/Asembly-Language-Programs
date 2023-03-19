abea2b01efd693dd6d2fd342b18faa18df8209a0
Ikram <115416397+Ikram001@users.noreply.github.com>
2023-03-19 18:54:43 +0500

    Add files via upload

---
+++ print A-Z capital letters.asm
@@ -0,0 +1,21 @@
+;print A-Z capital letters
+.model small
+.stack 100h
+.data
+.code
+main proc
+mov cx, 26
+mov dx,65
+
+
+L1:
+mov ah,2
+int 21h
+ADD dx,1
+loop L1 
+mov ah, 4ch
+int 21h   
+     
+    
+main endp
+end main
